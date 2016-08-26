9fe81db5efd35ccab63c4bdee75b56f3a65f5692
nuclemike <nuclemike@gmail.com>
2016-08-26 23:03:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-$("#loader, #splashLogo").addClass("loading");
-	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$("#loader").fadeOut("slow");
-	});
+$(document).ready(function() {
+	$("#loader, #splashLogo").addClass("loading");
+});
+
+// Wait for window load
+$(window).load(function() {
+	// Animate loader off screen
+	$("#loader").fadeOut("slow");
+});
