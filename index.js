1d1d4f359eb977d943899199a91bb5ae0372e525
nuclemike <nuclemike@gmail.com>
2016-08-26 23:14:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 $(window).load(function() {
 	setTimeout(function(){ 
 		$("#loader").remove();
-		$("splashLogo").attr('class', 'loaded');
+		$("#splashLogo").attr('class', 'loaded');
 		
 	}, 3000);
 	
