e777752665c722b44d92e0241cd96fa2291189da
nuclemike <nuclemike@gmail.com>
2016-12-12 13:56:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ $(window).load(function() {
 		$("#splashDiv").fadeOut();
 		
 		
-	}, 000);
+	}, 3000);
 	
 	
 });
 
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- //alert('mobile');
+if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ) {
+ $("body").addClass('mobile');
 }
-
 
 function toggleMenu() {
     $("#headerMenu").toggleClass("open");
