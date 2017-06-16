121b18f7e1ba70968121ea5b05f73ff0f6c41c56
nuclemike <nuclemike@gmail.com>
2017-06-16 13:59:44 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -7,12 +7,12 @@
         // url: "https://docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
         
     $.ajax({
-        url: "http://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
+        url: "http://docs.goxogle.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
         
         type: "POST",
         data: 
         {          
-          "entry.1565555327x": $("#liquidOrderCustomerName").text(),
+          "entry.1565555327": $("#liquidOrderCustomerName").text(),
           "entry.1619189758":$("#liquidOrderCustomerEmail").text(),
           "entry.904873223":"http://graph.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")+"/picture?width=800&height=800",
           "entry.1380378158":"http://www.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")
