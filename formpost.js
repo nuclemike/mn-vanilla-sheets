08342443404f9cf98092ce52d65f6e4c88d992bb
nuclemike <nuclemike@gmail.com>
2017-06-16 13:11:52 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -6,7 +6,7 @@
         
         //url: "http://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
     $.ajax({
-        url: "https://docs.google.com/a/mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
+        url: "https://docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
         
         type: "POST",
         data: 
