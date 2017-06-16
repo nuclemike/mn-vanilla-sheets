b22075235080402d66ecc0ca131fbbce04692957
nuclemike <nuclemike@gmail.com>
2017-06-16 12:07:45 +0200

    Update fbLogin.js

--- fbLogin.js
+++ fbLogin.js
@@ -38,8 +38,9 @@
       testAPI();
     } else {
       // The person is not logged into your app or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      document.getElementById('authorizationText').innerHTML = 'Unauthorized: Please login with Facebook.';
+      document.getElementById('authorizationText').className = "denied";
+      ocument.getElementById('liquidOrderCustomerName').innerHTML = 'Welcome dear Vaper!';
       document.getElementById('fbLogin').style.display = "block";
       document.getElementById('fbLogout').style.display = "none";
     }
