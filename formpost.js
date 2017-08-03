252c306a4030c300ac2df3e0a832d93e5981cf95
nuclemike <nuclemike@gmail.com>
2017-08-03 22:46:41 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -7,10 +7,7 @@
         // url: "https://docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
         
     $.ajax({
-        url: "http://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },       
+        url: "http://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",    
         crossDomain: true,
         type: "POST",
         data: 
