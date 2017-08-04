973f003e2542d6a46a8ec04aeafc055f7c694854
nuclemike <nuclemike@gmail.com>
2017-08-04 10:28:36 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -3,50 +3,6 @@
     var resultDiv = $("#resultDivContainer");
         
         
-        // url: "http://cors.io/docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
-        
-    $.ajax({
-        url: "http://cors.io/docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",    
-        type: "POST",
-        data: 
-          {        
-          "entry.1565555327": $("#liquidOrderCustomerName").text(),
-          "entry.1619189758":$("#liquidOrderCustomerEmail").text(),
-          "entry.904873223":"http://graph.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")+"/picture?width=800&height=800",
-          "entry.1380378158":"http://www.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")
-            }        
-        ,                
-        dataType: "xml",
-        statusCode:{
-            0: function(){
-                alert('bully');
-             },
-            200:function(){
-                alert('bully');
-             }
-        }    ,
-        success: function (result) {
-            switch (result) {
-                case true:
-                    console.log("success true :" +result);
-                    break;
-                default:
-                    console.log("success true :" +result);
-            }
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            console.log("status: "+xhr.status+" | error: "+thrownError);
-        
-        }
-    });
-};
-
-
-
-    JSONTestx = function() {
-    var resultDiv = $("#resultDivContainer");
-        
-        
         // url: "https://docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
         
     $.ajax({
