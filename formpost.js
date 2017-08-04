79dccc85d503c5bacf5f7d26d3cd312f4b4c1e6f
nuclemike <nuclemike@gmail.com>
2017-08-04 10:17:19 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -1,18 +1,50 @@
-var http = new XMLHttpRequest();
-JSONTest = function() {
 
-var url = "https://docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse";
-var params = "entry.1565555327=ipsum&entry.1619189758=binny";
-http.open("GET", url+"?"+params, true);
-http.onreadystatechange = function() {//Call a function when the state changes.
-	if(http.readyState == 4 && http.status == 200) {
-		alert(http.responseText);
-	}
-}
-http.send(null);
-    
-    }
-    
+    JSONTest = function() {
+    var resultDiv = $("#resultDivContainer");
+        
+        
+        // url: "http://cors.io/docs.google.com/a/www.mamasnectar.com/forms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
+        
+    $.ajax({
+        url: "http://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",    
+        "Access-Control-Allow-Origin": "http://www.mamasnectar.com",
+        "Origin": "http://www.mamasnectar.com",
+        type: "POST",
+        data: 
+          {        
+          "entry.1565555327": $("#liquidOrderCustomerName").text(),
+          "entry.1619189758":$("#liquidOrderCustomerEmail").text(),
+          "entry.904873223":"http://graph.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")+"/picture?width=800&height=800",
+          "entry.1380378158":"http://www.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")
+            }        
+        ,                
+        dataType: "xml",
+        statusCode:{
+            0: function(){
+                alert('bully');
+             },
+            200:function(){
+                alert('bully');
+             }
+        }    ,
+        success: function (result) {
+            switch (result) {
+                case true:
+                    console.log("success true :" +result);
+                    break;
+                default:
+                    console.log("success true :" +result);
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log("status: "+xhr.status+" | error: "+thrownError);
+        
+        }
+    });
+};
+
+
+
     JSONTestx = function() {
     var resultDiv = $("#resultDivContainer");
         
