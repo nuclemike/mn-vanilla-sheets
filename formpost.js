4624b794a920b187ebaf59ae0240f533f23737b2
nuclemike <nuclemike@gmail.com>
2017-08-05 23:57:20 +0200

    Update formpost.js

--- formpost.js
+++ formpost.js
@@ -1,16 +1,21 @@
 
-    JSONTest = function() {
+    formPost = function() {
     var resultDiv = $("#resultDivContainer");
         
-        
-        // url: "https://nl.hideproxy.me/go.php?u=https://docs.google.com/aforms/d/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",
-        
+    $('#sendOrder').addClass('denied').text('sending...');    
     $.ajax({
         url: "https://thingproxy.freeboard.io/fetch/https://docs.google.com/forms/d/e/1FAIpQLSeCCI4TI1CVp-6FgzFk4_XRsgUa9cTDVzUK0dg_1U-gHFzkzQ/formResponse",    
         type: "POST",
         data: 
-          {        
-          "entry.1565555327": $("#liquidOrderCustomerName").text(),
+          {                   
+              "entry.1565555327": $("#liquidOrderCustomerName").text(),
+              "entry.1212940529": $("#liquidOrderFlavor").text(),
+              "entry.1380378158": $("#liquidOrderCustomerProfile").text(),
+              "entry.1619189758": $("#liquidOrderCustomerEmail").text(),
+              "entry.601560370": $("#liquidOrderSize").text(),
+              "entry.1250591470": $("#liquidOrderNicotine").text(),
+              "entry.1798744401": $("#liquidOrderVG").text(),
+              
           "entry.1619189758":$("#liquidOrderCustomerEmail").text(),
           "entry.904873223":"http://graph.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")+"/picture?width=800&height=800",
           "entry.1380378158":"http://www.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid")
@@ -19,19 +24,20 @@
         dataType: "xml",
         statusCode:{
             0: function(){
-                alert('bully');
+                alert('bully 1');
              },
             200:function(){
-                alert('bully');
+                alert('t alla!');
+                    $('#sendOrder').removeClass('denied').text('sending...');    
              }
         }    ,
         success: function (result) {
             switch (result) {
                 case true:
-                    console.log("success true :" +result);
+                    //console.log("success true :" +);
                     break;
                 default:
-                    console.log("success true :" +result);
+                    //console.log("success true :" +result);
             }
         },
         error: function (xhr, ajaxOptions, thrownError) {
