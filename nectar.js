b3850beab17bc9828064248d0c6b41d1e8e3ca90
nuclemike <nuclemike@gmail.com>
2017-09-26 00:07:46 +0200

    Create nectar.js

---
+++ nectar.js
@@ -0,0 +1,7 @@
+// window.localStorage.setItem('name', response.name);
+//	  window.localStorage.setItem('email', response.email);
+//	  window.localStorage.setItem('pass', response.pass);
+//	  window.localStorage.setItem('mobile', response.mobile);
+      
+document.getElementById('liquidOrderCustomerName').innerHTML = 'Welcome '+localStorage.getItem("name")+'!';
+document.getElementById('liquidOrderCustomerEmail').innerHTML = localStorage.getItem("email");
