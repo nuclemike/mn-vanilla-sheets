87de19a26975aefa556b57fd3920ae7b0c2d04ed
nuclemike <nuclemike@gmail.com>
2017-09-27 06:46:00 +0200

    Update index.js

--- spa/index.js
+++ spa/index.js
@@ -51,6 +51,7 @@ function labRequestPopup(nectarName) {
 	document.getElementById("liquidOrderNectarName").innerHTML = nectarName;
 	document.getElementById('liquidOrderCustomerName').innerHTML = sessionStorage.getItem("name");
 	document.getElementById('liquidOrderCustomerEmail').innerHTML = sessionStorage.getItem("email");   
+	document.getElementById('totalCostValue').innerHTML = "€0.00";
 	document.getElementById('liquidOrderCustomerNickname').value = sessionStorage.getItem("name");
 	document.getElementById('liquidOrderQuantity').value = 1;
 	document.getElementById('liquidOrderSize').value = '';
