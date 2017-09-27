a407243bf996174811fb491d4519f51162f6bfae
nuclemike <nuclemike@gmail.com>
2017-09-27 06:06:54 +0200

    Update formpost.js

--- spa/formpost.js
+++ spa/formpost.js
@@ -1,5 +1,5 @@
   // Make an AJAX call to Google Script
-  function test() {
+  function formPcccost() {
     
     var url = "https://script.google.com/macros/s/AKfycby2yL0OxsZFDEzESDNHBTqGdZuZHwCzkbBjQ6_prTtZDt3iHACU/exec?callback=testCb";
 
@@ -21,14 +21,14 @@
   }
 
   // print the returned data
-function testCb(response) {
+function tecccstCb(response) {
 	console.log(response)
 	
 }
 
 
 
-    formPost = function() {
+  function formPost() {
       if ($("#liquidOrderCustomerNickname").val()=="") {
         $("#liquidOrderCustomerNickname").addClass("requiredField")}
         else { $("#liquidOrderCustomerNickname").removeClass("requiredField")}
@@ -52,54 +52,45 @@ function testCb(response) {
      if ($( ".requiredField" ).length) {return false};
 
      
-    $('#sendOrder').hide();
-        $('#cancelOrder').css('opacity','0');
-        $('#cancelOrder').attr('disabled');
+    $('#sendOrder, #cancelOrder').hide();
+        
         $('#sendingPanel').fadeIn('medium');
         
         
-    $.ajax({
-        url: "https://thingproxy.freeboard.io/fetch/https://docs.google.com/forms/d/e/1FAIpQLSeoSGSuoK1Fybl7W0Jw45NZct4hdS3E_sK8S7aPslIfqM48Zg/formResponse",    
-        type: "POST",
-        data: 
-          {                   
-              "entry.993384798": $("#liquidOrderCustomerName").text(),
-              "entry.69264499": $("#liquidOrderNectarName").text(),
-              "entry.859530884": "http://www.facebook.com/"+$("#liquidOrderCustomerName").attr("fbid"),
-              "entry.591208937": $("#liquidOrderCustomerEmail").val(),
-              "entry.48792472": $("#liquidOrderCustomerNickname").val(),
-              "entry.1844516169": $("#liquidOrderSize").val(),
-              "entry.1234826815": $("#liquidOrderNicotine").val(),
-              "entry.372041597": $("#liquidOrderVG").val(),
-              "entry.600498963": $("#liquidOrderQuantity").val()
-              
-            }        
-        ,                
-        statusCode:{
-            0: function(){
-               
-             },
-            200:function(){
-              $('#orderSentPanel b').text($("#liquidOrderCustomerEmail").val());
-                    $('#orderSentPanel').fadeIn('medium', function() {
-       $('#cancelOrder').removeAttr('disabled');
-                        $('#cancelOrder').css('opacity','1');
-        $('#fbLogout').show();
-  });
-             }
-        }    ,
-        success: function (result) {
-            switch (result) {
-                case true:
-                    //console.log("success true :" +);
-                    break;
-                default:
-                    //console.log("success true :" +result);
-            }
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            console.log("status: "+xhr.status+" | error: "+thrownError);
-        
-        }
+  var url = "https://script.google.com/macros/s/AKfycby2yL0OxsZFDEzESDNHBTqGdZuZHwCzkbBjQ6_prTtZDt3iHACU/exec?callback=postRqCb";
+
+	var flavor = 'a';//$('#loginPopupEmail').val();
+	var email = 'b';//$('#loginPopupPassword').val();
+	var size = 'c';//$('#loginPopupPassword').val();
+	var nicotine = 'd';//$('#loginPopupPassword').val();
+	var vg = 'e';//$('#loginPopupPassword').val();
+	var qty = 'f';//$('#loginPopupPassword').val();
+	var nickname = 'g';//$('#loginPopupPassword').val();
+
+    var request = jQuery.ajax({
+      crossDomain: true,
+      url: url + '&flv='+encodeURIComponent(flavor)+'&eml='+encodeURIComponent(email)+'&siz='+encodeURIComponent(email)+'&nct='+encodeURIComponent(email)+'&vgr='+encodeURIComponent(email)+'&qty='+encodeURIComponent(email)+'&nnm='+encodeURIComponent(email),
+      method: "GET",
+      dataType: "jsonp"
     });
-};
+
+  }
+
+  // print the returned data
+function postRqCb(response) {
+	if (response.success) {
+		$('#orderSentPanel b').text($("#liquidOrderCustomerEmail").val());
+		$('#orderSentPanel').fadeIn('medium');
+		$('#cancelOrder').show();
+	}
+	else {
+		
+		alert('error');
+		$('#cancelOrder').show();
+
+	}
+} 
+	  
+	  
+	  
+
