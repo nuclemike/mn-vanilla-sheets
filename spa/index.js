3c8f2203b8d577ffc80e9d1623423c5b07ba631c
nuclemike <nuclemike@gmail.com>
2017-09-27 11:07:16 +0200

    Update index.js

--- spa/index.js
+++ spa/index.js
@@ -2,30 +2,6 @@ $(document).ready(function() {
 	loadContent('nectars');	
 });
 
-// Wait for window load
-/*
-$(window).load(function() {
-	setTimeout(function(){ 
-		$("#splashDiv").slideUp('slow');
-		//$(".content").show();
-	}, 0);		
-});
-
-
-
-if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ) 	
-	{
- 	$("body").addClass('mobile');
-	}
-*/
-
 function pageLoaded() {
 	$("#pageLoader").hide();
 	$( "#pageContent" ).show()
