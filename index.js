3b5932912fad220e7ef346f90926d7cbf559742e
nuclemike <nuclemike@gmail.com>
2018-11-17 17:50:49 +0100

    s

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function loadContent(pageName) {
 	else if (pageName == 'nectars')
 		$( "#headerNectar" ).addClass('selected').siblings().removeClass('selected')
 	
-	$( "#pageContent" ).load( pageName+".html?v=sep2013", function() {
+	$( "#pageContent" ).load( pageName+".html", function() {
 		//after load html (images excluded)
 		
 	});
