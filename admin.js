e4cb67185295a5650f40847873cf66706daed4d0
Mike Gatt <nuclemike@gmail.com>
2019-01-08 14:35:18 +0100

    w

--- admin.js
+++ admin.js
@@ -15,22 +15,68 @@ function fetchProductionCb(e) {
 
 	if (e.error) { alert(e.message) }
 	else {
-		var html = '';
 
-		html += '<tr>';
-		html += '<th>qty</th><th>flavor</th><th>size</th><th>nicotine</th><th>nicType</th>';
-		html += '</tr>';
+		var array = [];
+		var shots = 0;
 
 		e.requests.forEach( function (item){
 
-			html += '<tr>';
-			html += '<td>'+item.quantity+'</td><td>'+item.flavor+'</td><td>'+item.size+'</td><td>'+item.nicotine+'</td><td>'+item.nicType+'</td>';
-			html += '</tr>';			
-
+			var found = false;
+				
+			for (var i = 0; i < array.length; i++) {			
+				if (array[i].flavor === item.flavor) {								
+					array[i][item.size] += item.quantity;									
+					found = true;
+				}
+			}
+				
+			if (!found) {
+				array.push({flavor : item.flavor, '30':0, '60':0, '120':0})
+				array[array.length-1][item.size] = item.quantity;
+			}
+			
+			shots += item.quantity *  Math.ceil( (item.nicotine / 72 * item.size ) / ( 24 / 72 * 10 ) );
+			
 		});
+		
+		
+		var html = '<tr>';
+		html += '<th>Flavor</th><th>30</th><th>60</th><th>120</th>';
+		html += '</tr>';
+		
+		var count = {'30':0,'60':0,'120':0};
+		
+		array.forEach( function (item){
 
+			html += '<tr>';
+			html += '<td>'+item.flavor+'</td><td>'+item['30']+'</td><td>'+item['60']+'</td><td>'+item['120']+'</td>';
+			html += '</tr>';		
+
+			count['30'] += item['30'];
+			count['60'] += item['60'];
+			count['120'] += item['120'];
+			
+			
+		});	
+
+			html += '<tr>';
+			html += '<td>Total Shots : '+shots+'</td><td>'+count['30']+'</td><td>'+count['60']+'</td><td>'+count['120']+'</td>';
+			html += '</tr>';		
+		
 		document.getElementById("productionTable").innerHTML = html;
 	}
 	
+}
+
+function containsFlavor(flavor, array) {
+    var i;
+
+	for (i = 0; i < array.length; i++) {			
+		if (array[i].flavor === flavor) {
+			return true;
+		}
+	}
+
+    return false;
 }
 
