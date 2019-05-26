10c768ee5d1a5245d11d5e5d64d7a361d9b1be6d
nuclemike <nuclemike@gmail.com>
2019-05-26 14:32:51 +0200

    e345

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function loadContent(pageName) {
 	else if (pageName == 'myaccount')
 		$( "#headerMyAccount" ).addClass('selected').siblings().removeClass('selected')	
 	
-	$( "#pageContent" ).load( pageName+".html?v=10", function() {
+	$( "#pageContent" ).load( pageName+".html?v=11", function() {
 		//after load html (images excluded)
 		
 	});
