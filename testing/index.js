bc420cf37c1f37e57ad930421d126428e0926ff4
nuclemike <nuclemike@gmail.com>
2019-07-13 18:38:47 +0200

    d

--- testing/index.js
+++ testing/index.js
@@ -65,7 +65,7 @@ function loadContent(pageName) {
 	else if (pageName == 'myaccount')
 		$( "#headerMyAccount" ).addClass('selected').siblings().removeClass('selected')	
 	
-	$( "#pageContent" ).load( pageName+".html?v=16", function() {
+	$( "#pageContent" ).load( pageName+".html?v=17", function() {
 		//after load html (images excluded)
 		
 	});
